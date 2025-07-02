4aa8431eeb8f657225e4cd97138ac75f985124de
Mao Soldevilla <maolenin@gmail.com>
2025-07-02 20:00:20 +0900

    Scope of vars

---
+++ 02jscourstesting/scope_lab.js
@@ -0,0 +1,31 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+
+console.log(globalVar); //Output: "I'm a global variable"
+console.log(globalLet); //Output: "I'm also global, but scoped with let";
+console.log(globalConst); //Output: "I'm a global const"
+
+//console.log(blockVar);
+//console.log(blockLet);
+//console.log(blockConst); 
+
+fucntion show(){
+    var fucntionVar = "I'm a block-scoped var";
+    let fucntionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+show();
+
+console.log(functionVar); //throws ReferenceError
+console.log(functionLet); //throws ReferenceError
+console.log(functionConst); //throws ReferenceError
