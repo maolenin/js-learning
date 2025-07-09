783137ad7a7c13e3a5c8650d2c5667324976ae83
Mao Soldevilla <maolenin@gmail.com>
2025-07-08 22:35:18 -0500

    Finishing exercise Menu

--- 08RestaurantMenu/restaurant_menu.js
+++ 08RestaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12.99', 'Eggs Benedict - $22.99', 'Oatmeal - $ 21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $25', 'Pasta - $18.15', 'Burger - $8', 'Salmon - $51.99'];
+const dessertMenu = ['Cake - $4.99', 'Ice Cream - $2', 'Pudding - $5', 'Fruit Salad - $12.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
