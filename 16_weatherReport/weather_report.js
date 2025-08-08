fa789bd835384b0adff067b40d138cf55e6ad077
Mao Soldevilla <maolenin@gmail.com>
2025-08-08 22:06:12 +0800

    Adding weather report

---
+++ 16_weatherReport/weather_report.js
@@ -0,0 +1,28 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = 'YOUR_IP_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            return response.json();
+//            console.log('aqui estas')
+            })
+        .then(data => {
+            console.log('Data from the API:', data);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+            <p>Temperature: ${data.main.temp} &#8451;</p>
+            <p>Weather: ${data.weather[0].description}</p>`;
+            //console.log('Data from the API:', data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
+
+
+    document.getElementById('weatherForm').addEventListener('submit',showweatherDetails);
