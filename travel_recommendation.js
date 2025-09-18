6bf6e0bb20367170b23aae20dc94d472985b44d8
Mao Soldevilla <maolenin@gmail.com>
2025-09-18 21:36:06 +0800

    Updating the Final Project and re-organizing the folders

---
+++ travel_recommendation.js
@@ -0,0 +1,86 @@
+const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
+
+function search_destination(event) {
+    event.preventDefault();
+    const input = document.getElementById('search').value.toLowerCase();    
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';    
+
+    fetch('travel_recommendation_api.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        let itemsToDisplay = [];
+        let categoryFound = false;
+        
+        switch (input) {
+            case 'countries':
+            case 'country':
+                itemsToDisplay = data.countries;
+                categoryFound = true;
+                break;
+            case 'beach':
+            case 'beaches':
+                itemsToDisplay = data.beaches;
+                categoryFound = true;
+                break;
+            case 'temple':
+            case 'temples':
+                itemsToDisplay = data.temples;
+                categoryFound = true;
+                break;
+            default:
+                alert("Sorry, We don't have that category");
+                console.log('Didnt find')
+        }
+        console.log(itemsToDisplay);      
+        if (categoryFound) {
+            resultDiv.innerHTML = '';
+
+            if (input.startsWith('countr')) {   
+                itemsToDisplay.forEach(item => {
+                const itemDiv = document.createElement('div');
+
+                item.cities.forEach( city => {
+                    itemDiv.className = 'item-card'; // Add a class for styling
+                    itemDiv.innerHTML = `
+                    <h3>${city.name}</h3>
+                    <img src="${city.imageUrl}">
+                    <p>${city.description}</p>
+                    `;
+                    resultDiv.appendChild(itemDiv);
+                    });
+                });           
+            }
+            else {
+                console.log('You are here')
+                itemsToDisplay.forEach(item => {
+                const itemDiv = document.createElement('div');
+                itemDiv.className = 'item-card'; // Add a class for styling
+                itemDiv.innerHTML = `
+                    <h3>${item.name}</h3>
+                    <img src="${item.imageUrl}">
+                    <p>${item.description}</p>
+                `;
+                resultDiv.appendChild(itemDiv);
+                });
+            }
+        } else {            
+            resultDiv.innerHTML = 'Condition not found.';
+        }      
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });    
+}
+
+btnSearch.addEventListener('click', search_destination);
+//console.log(data);
+
+
